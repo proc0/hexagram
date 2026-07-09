2aba4e4afaf2fb90aa5bdfa1bf2c671c14a966da
proc0 <alanvx@gmail.com>
2026-07-09 03:32:35 -0700

    adding new grid, adding hex grid operations, adding basic directions and basic hex direction test

--- include/grid.hpp
+++ include/grid.hpp
@@ -5,20 +5,102 @@
 
 #include <raylib.h>
 
+#include <unordered_map>
+#include <array>
+
+#define HEX_SIZE 60.0f
+
+struct HexPoint { 
+    // cube coordinates storage 
+    int q, r, s;
+    // axial coordinates constructor
+    // derive the third coordinate s by -q - r
+    HexPoint(int q1, int r1): q(q1), r(r1), s(-q1 - r1) {}
+    HexPoint(int q1, int r1, int s1): q(q1), r(r1), s(s1) {}
+    bool operator==(const HexPoint&) const = default;
+};
+
+struct HexState {
+  HexPoint point;
+  Vector2 position;
+  bool isActive = false;
+};
+
+struct Matrix2x2Pair {
+    const float f0, f1, f2, f3;
+    const float b0, b1, b2, b3;
+};
+
+// https://www.redblobgames.com/grids/hexagons/implementation.html
+namespace std {
+    template <> struct hash<HexPoint> {
+        size_t operator()(const HexPoint& h) const {
+            hash<int> int_hash;
+            size_t hq = int_hash(h.q);
+            size_t hr = int_hash(h.r);
+            return hq ^ (hr + 0x9e3779b9 + (hq << 6) + (hq >> 2));
+        }
+    };
+}
+
+enum HexDirection {
+  UP,
+  UP_RIGHT,
+  DOWN_RIGHT,
+  DOWN,
+  DOWN_LEFT,
+  UP_LEFT,
+};
+
 class Grid : public Layer {
+  std::unordered_map<HexPoint, HexState> map;
+
+  // flat top
+  const Matrix2x2Pair view = Matrix2x2Pair({
+    3.0f/2.0f, 0.0f, sqrtf(3.0f)/2.0f, sqrtf(3.0f),
+    2.0f/3.0f, 0.0f, -1.0f/3.0f, sqrtf(3.0f)/3.0f
+  });
+
+  const std::array<HexPoint, 6> directions = {
+    HexPoint(0, -1, 1), HexPoint(1, -1, 0), HexPoint(1, 0, -1),   
+    HexPoint(0, 1, -1), HexPoint(-1, 1, 0), HexPoint(-1, 0, 1)
+  };
+  // pointy top
+  // const Matrix2x2Pair view = Matrix2x2Pair({
+  //   sqrtf(3.0f), sqrtf(3.0f)/2.0f, 0.0f, 3.0f/2.0f,
+  //   sqrtf(3.0f)/3.0f, -1.0f/3.0f, 0.0, 2.0f/3.0f
+  // });
+  HexPoint activeHex = HexPoint(0, 0, 0);
 
   const Window& window;
+
+  Vector2 origin = { window.halfWidthf, window.halfHeightf };
+  float hexSize = HEX_SIZE;
 
 public:
     Grid(const Window& window): window(window) {};
     ~Grid() = default;
 
     void load();
-    
+    void generate(int layers);
+
     void renderGrid() const;
+    void renderHex(const HexState&) const;
+
     void updateGrid();
+    void updateHex(HexDirection dir);
+
+    HexPoint inject(Vector2 point);
+    Vector2 project(HexPoint);
 
     void resize(int width, int height) override;
+
+    HexPoint hexAdd(HexPoint a, HexPoint b) const;
+    HexPoint hexSubtract(HexPoint a, HexPoint b) const;
+    HexPoint hexMultiply(HexPoint a, int k) const;
+    int hexLength(HexPoint a) const;
+    int hexDistance(HexPoint a, HexPoint b) const;
+    HexPoint hexNeighbor(HexPoint hex, HexDirection dir) const;
     // void transition(State::Screen);
     // void unload();
 };
--- src/grid.cpp
+++ src/grid.cpp
@@ -3,17 +3,103 @@
 #include <raylib.h>
 
 void Grid::load() {
+	generate(3);
 
+	map.at(activeHex).isActive = true;
+}
+
+// k = number of hex layers
+void Grid::generate(int k) {
+	for (int q = -k; q <= k; q++) {
+	    int r1 = fmax(-k, -q - k);
+	    int r2 = fmin( k, -q + k);
+	    // bool isRowEdge = q == -k || q == k;
+	    for (int r = r1; r <= r2; r++) {
+	    	HexPoint hex = { q, r };
+	    	// bool isColEdge = r == r1 || r == r2;
+			// map[hex] = { hex, project(hex) };
+			map.insert({ hex, { hex, project(hex) } });
+	    }
+	}
 }
 
 void Grid::renderGrid() const {
+	for (auto& [point, hex] : map ) {
+		renderHex(hex);
+	}
+}
 
+void Grid::renderHex(const HexState& hex) const {
+    DrawPoly(hex.position, 6, hexSize, 0.0f, hex.isActive ? YELLOW : BEIGE);
+    DrawPolyLines(hex.position, 6, hexSize, 0.0f, BLACK);
+    const char* pointLabel = TextFormat("(%d, %d, %d)", hex.point.q, hex.point.r, hex.point.s);
+    DrawText(pointLabel, hex.position.x-30.0f, hex.position.y-9.0f, 18, BLACK);
 }
 
 void Grid::updateGrid() {
+	if (IsKeyPressed(KEY_W)) {
+		updateHex(HexDirection::UP);
+	} else if (IsKeyPressed(KEY_E)) {
+		updateHex(HexDirection::UP_RIGHT);
+	} else if (IsKeyPressed(KEY_D)) {
+		updateHex(HexDirection::DOWN_RIGHT);
+	} else if (IsKeyPressed(KEY_S)) {
+		updateHex(HexDirection::DOWN);
+	} else if (IsKeyPressed(KEY_A)) {
+		updateHex(HexDirection::DOWN_LEFT);
+	} else if (IsKeyPressed(KEY_Q)) {
+		updateHex(HexDirection::UP_LEFT);
+	}
+}
 
+void Grid::updateHex(HexDirection dir) {
+	map.at(activeHex).isActive = false;
+	HexPoint nextHex = hexNeighbor(activeHex, dir);
+	activeHex = nextHex;
+	map.at(activeHex).isActive = true;
+}
+
+HexPoint Grid::inject(Vector2 point) {
+    HexPoint hex = HexPoint(
+    	(point.x - origin.x) / hexSize, 
+        (point.y - origin.y) / hexSize);
+    float q = view.b0 * hex.q + view.b1 * hex.r;
+    float r = view.b2 * hex.q + view.b3 * hex.r;
+
+	return { static_cast<int>(roundf(q)), static_cast<int>(roundf(r)) };
+}
+
+Vector2 Grid::project(HexPoint hexPoint) {
+    float x = (view.f0 * hexPoint.q + view.f1 * hexPoint.r) * hexSize;
+    float y = (view.f2 * hexPoint.q + view.f3 * hexPoint.r) * hexSize;
+	
+	return { x + origin.x, y + origin.y };
 }
 
 void Grid::resize(int width, int height) {
 
+}
+
+HexPoint Grid::hexAdd(HexPoint a, HexPoint b) const {
+    return HexPoint(a.q + b.q, a.r + b.r, a.s + b.s);
+}
+
+HexPoint Grid::hexSubtract(HexPoint a, HexPoint b) const {
+    return HexPoint(a.q - b.q, a.r - b.r, a.s - b.s);
+}
+
+HexPoint Grid::hexMultiply(HexPoint a, int k) const {
+    return HexPoint(a.q * k, a.r * k, a.s * k);
+}
+
+int Grid::hexLength(HexPoint a) const {
+    return int((abs(a.q) + abs(a.r) + abs(a.s)) / 2);
+}
+
+int Grid::hexDistance(HexPoint a, HexPoint b) const {
+    return hexLength(hexSubtract(a, b));
+}
+
+HexPoint Grid::hexNeighbor(HexPoint hex, HexDirection dir) const {
+    return hexAdd(hex, directions[dir]);
 }
